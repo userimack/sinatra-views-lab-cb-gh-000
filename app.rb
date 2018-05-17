806f1ed7b30e25b0787e4418ec422235a0819365
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-17 08:41:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 class App < Sinatra::Base
 
 	get '/' do
-		erb :index
+		erb :hello
 	end
-
-
+	
+	get '/hello' do 
+	  erb :hello
+	end 
+	
+	get '/goodbye' do 
+	  erb :goodbye 
+	end 
+	
+	get '/date' do 
+	  erb :date 
+	end 
 end
